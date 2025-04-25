e973417122d3b0656f4bb086dd10b999729d3c98
The-GO0d-Guy <malaha814@gmail.com>
2025-04-25 19:15:13 +0530

    feat: filter panel is scrollable

--- src/components/Filters/FilterPanel.jsx
+++ src/components/Filters/FilterPanel.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import ConsultationFilter from './ConsultationFilter';
-import SpecialtyFilter from './SpecialtyFilter';
-import SortFilter from './SortFilter';
+import React from "react";
+import ConsultationFilter from "./ConsultationFilter";
+import SpecialtyFilter from "./SpecialtyFilter";
+import SortFilter from "./SortFilter";
 
 export default function FilterPanel({
-  mode, onModeChange,
-  specialties, selectedSpecs, onSpecsChange,
-  sortBy, onSortChange
+  mode,
+  onModeChange,
+  specialties,
+  selectedSpecs,
+  onSpecsChange,
+  sortBy,
+  onSortChange,
 }) {
   return (
     <aside className="w-64 space-y-6 sticky top-4 self-start">
-      <div className="bg-white p-4 rounded-xl shadow space-y-4">
+      <div
+        className="bg-white p-4 rounded-xl shadow space-y-4
+                   max-h-[40vh] overflow-y-auto scrollbar-hide"
+      >
         <ConsultationFilter mode={mode} onChange={onModeChange} />
         <SortFilter sortBy={sortBy} onChange={onSortChange} />
       </div>
-      <div className="bg-white p-4 rounded-xl shadow">
+      <div
+        className="bg-white p-4 rounded-xl shadow
+                   max-h-[50vh] overflow-y-auto scrollbar-hide"
+      >
         <SpecialtyFilter
           specialties={specialties}
           selected={selectedSpecs}
