415c79e515cf56394239bee097515a093530c08d
The-GO0d-Guy <malaha814@gmail.com>
2025-04-25 17:09:48 +0530

    chore: first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Doctor Listing
+      </h1>
+    </div>
+  )
+}
